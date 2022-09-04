5aed60d5b1e59f1b4a98f015e69872a413b78551
DanielAF07 <67185475+DanielAF07@users.noreply.github.com>
2022-09-04 01:18:53 -0500

    Delete unused code

--- src/components/StyledSelect.jsx
+++ src/components/StyledSelect.jsx
@@ -1,23 +1,14 @@
-
-const options = [
-  {
-    value: 'bienes',
-    label: 'Bienes y servicios adquiridos con criptomonedas'
-  },
-
-]
-
-function StyledSelect({label, placeholder, options, onChange, value, name}) {
+function StyledSelect ({ label, placeholder, options, onChange, value, name }) {
   return (
-    <div className="flex flex-col mb-4">
-      <p className="text-left mb-2">{label}</p>
-      <select 
-        className="form-select bg-white/20 border-none rounded-xl py-2"
+    <div className='flex flex-col mb-4'>
+      <p className='text-left mb-2'>{label}</p>
+      <select
+        className='form-select bg-white/20 border-none rounded-xl py-2'
         onChange={onChange}
         value={value}
         name={name}
       >
-        {placeholder && <option value="" disabled>{placeholder}</option>}
+        {placeholder && <option value='' disabled>{placeholder}</option>}
         {options && (
           options.map(option => (
             <option key={option.value} value={option.label}>{option.label}</option>
